fecca1d52089bc0b93c455513de858c9dede9814
Shnupbups <shnupbups@gmail.com>
2020-06-13 02:40:55 +1000

    Delete ArmorItemMixin.java

--- src/main/java/com/github/chainmailstudios/astromine/mixin/ArmorItemMixin.java
+++
@@ -1,9 +0,0 @@
-package com.github.chainmailstudios.astromine.mixin;
-
-import org.spongepowered.asm.mixin.Mixin;
-
-import net.minecraft.item.ArmorItem;
-
-@Mixin(ArmorItem.class)
-public class ArmorItemMixin {
-}
