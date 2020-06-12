d8f8d5ae569d67d94e56eb183222d3621ac58d30
vini2003 <vini.sch@outlook.com>
2020-06-12 17:56:41 -0300

    Fire Extinguisher.

---
+++ src/main/java/com/github/chainmailstudios/astromine/tool/FireExtinguisher.java
@@ -0,0 +1,60 @@
+package com.github.chainmailstudios.astromine.tool;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FireBlock;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+import java.util.stream.Stream;
+
+// TODO: Extinguish entities.
+public class FireExtinguisher extends Item {
+	public static final Item.Settings SETTINGS = new Item.Settings().maxCount(1);
+
+	public FireExtinguisher() {
+		super(SETTINGS);
+	}
+
+	@Override
+	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+		Vec3d placeVec = user.getCameraPosVec(0);
+
+		Vec3d thrustVec = new Vec3d(0.5, 0.5, 0.5);
+
+		thrustVec = thrustVec.multiply(user.getRotationVector());
+
+		for (int i = 0; i < world.random.nextInt(64); ++i) {
+			float r = world.random.nextFloat();
+			world.addParticle(ParticleTypes.LARGE_SMOKE, placeVec.x + thrustVec.x, placeVec.y + thrustVec.y, placeVec.z + thrustVec.z, thrustVec.x * r, thrustVec.y * r, thrustVec.z * r);
+		}
+
+		thrustVec = thrustVec.multiply(-1);
+
+		if (!user.isSneaking()) {
+			user.addVelocity(thrustVec.x, thrustVec.y, thrustVec.z);
+		}
+
+		BlockHitResult result = (BlockHitResult) user.rayTrace(6, 0, false);
+
+		BlockPos.Mutable.method_29715(new Box(result.getBlockPos()).expand(1)).forEach(position -> {
+			BlockState state = world.getBlockState(position);
+
+			state.getEntries().keySet().stream().filter(property -> property == FireBlock.EAST || property == FireBlock.WEST || property == FireBlock.NORTH || property == FireBlock.SOUTH || property == FireBlock.UP).forEach(property ->
+					world.setBlockState(position, Blocks.AIR.getDefaultState()));
+		});
+
+		return super.use(world, user, hand);
+	}
+}
