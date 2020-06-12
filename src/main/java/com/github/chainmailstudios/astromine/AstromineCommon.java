f988152cc074a893407c25ecee2ce810a3bb0d09
SuperCoder7979 <k.pranav@gmail.com>
2020-06-12 12:44:04 -0400

    basic dimension

--- src/main/java/com/github/chainmailstudios/astromine/AstromineCommon.java
+++ src/main/java/com/github/chainmailstudios/astromine/AstromineCommon.java
@@ -1,10 +1,15 @@
 package com.github.chainmailstudios.astromine;
 
 import blue.endless.jankson.Jankson;
+import com.github.chainmailstudios.astromine.world.gen.AstromineBiomeSource;
+import com.github.chainmailstudios.astromine.world.gen.AstromineChunkGenerator;
 import com.google.gson.Gson;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class AstromineCommon implements ModInitializer {
 	public static final String LOG_ID = "Astromine";
@@ -17,6 +22,7 @@ public class AstromineCommon implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// Unused.
+		Registry.register(Registry.BIOME_SOURCE, new Identifier(MOD_ID, MOD_ID), AstromineBiomeSource.CODEC);
+		Registry.register(Registry.CHUNK_GENERATOR, new Identifier(MOD_ID, MOD_ID), AstromineChunkGenerator.CODEC);
 	}
 }
