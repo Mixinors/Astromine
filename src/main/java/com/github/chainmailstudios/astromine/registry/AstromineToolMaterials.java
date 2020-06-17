6f5f5cc6c48ad23298ac5fd3f040af382f48eb2c
Shnupbups <shnupbups@gmail.com>
2020-06-18 01:10:58 +1000

    Patchouli Part 2
    Finished the materials chapter, made basic outlines for the other chapters
    Also made more tweaks to tool and armor materials

--- src/main/java/com/github/chainmailstudios/astromine/registry/AstromineToolMaterials.java
+++ src/main/java/com/github/chainmailstudios/astromine/registry/AstromineToolMaterials.java
@@ -8,8 +8,8 @@ import net.minecraft.util.Lazy;
 
 public class AstromineToolMaterials {
 	public static final AstromineToolMaterial METITE = new AstromineToolMaterial(2, 981, 14f, 5.0f, 5, () -> Ingredient.ofItems(AstromineItems.METITE_INGOT));
-	public static final AstromineToolMaterial ASTERITE = new AstromineToolMaterial(5, 2643, 10f, 5.0f, 20, () -> Ingredient.ofItems(AstromineItems.ASTERITE_FRAGMENT));
-	public static final AstromineToolMaterial STELLUM = new AstromineToolMaterial(5, 2015, 8f, 6.0f, 15, () -> Ingredient.ofItems(AstromineItems.STELLUM_INGOT));
+	public static final AstromineToolMaterial ASTERITE = new AstromineToolMaterial(5, 2015, 10f, 5.0f, 20, () -> Ingredient.ofItems(AstromineItems.ASTERITE_FRAGMENT));
+	public static final AstromineToolMaterial STELLUM = new AstromineToolMaterial(5, 2643, 8f, 6.0f, 15, () -> Ingredient.ofItems(AstromineItems.STELLUM_INGOT));
 	public static final AstromineToolMaterial GALAXIUM = new AstromineToolMaterial(6, 3072, 11f, 5.0f, 18, () -> Ingredient.ofItems(AstromineItems.GALAXIUM_FRAGMENT));
 	public static final AstromineToolMaterial UNIVITE = new AstromineToolMaterial(7, 3918, 12f, 6.0f, 22, () -> Ingredient.ofItems(AstromineItems.UNIVITE_INGOT));
 
