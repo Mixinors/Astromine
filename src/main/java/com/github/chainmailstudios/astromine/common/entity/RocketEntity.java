83a3af0cdcefc8c19b89a33c273177469fbe6b04
Draylar <samuelhimes@gmail.com>
2020-06-18 21:47:20 -0500

    rocket spawn mechanics

--- src/main/java/com/github/chainmailstudios/astromine/common/entity/RocketEntity.java
+++ src/main/java/com/github/chainmailstudios/astromine/common/entity/RocketEntity.java
@@ -1,12 +1,19 @@
 package com.github.chainmailstudios.astromine.common.entity;
 
+import com.github.chainmailstudios.astromine.AstromineCommon;
+import io.netty.buffer.Unpooled;
+import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Packet;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 
 public class RocketEntity extends Entity {
+
+    public static final Identifier ROCKET_SPAWN = AstromineCommon.identifier("rocket_spawn");
 
     public RocketEntity(EntityType<?> type, World world) {
         super(type, world);
@@ -29,6 +36,12 @@ public class RocketEntity extends Entity {
 
     @Override
     public Packet<?> createSpawnPacket() {
-        return null;
+        PacketByteBuf packet = new PacketByteBuf(Unpooled.buffer());
+
+        packet.writeBlockPos(this.getBlockPos());
+        packet.writeUuid(this.getUuid());
+        packet.writeInt(this.getEntityId());
+
+        return ServerSidePacketRegistry.INSTANCE.toPacket(ROCKET_SPAWN, packet);
     }
 }
--- src/main/java/com/github/chainmailstudios/astromine/registry/AstromineClientPackets.java
+++ src/main/java/com/github/chainmailstudios/astromine/registry/AstromineClientPackets.java
@@ -1,11 +1,18 @@
 package com.github.chainmailstudios.astromine.registry;
 
+import com.github.chainmailstudios.astromine.common.entity.RocketEntity;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
 
 import net.minecraft.util.Identifier;
 
 import com.github.chainmailstudios.astromine.AstromineCommon;
+import net.minecraft.util.math.BlockPos;
 
+import java.util.UUID;
+
+@Environment(EnvType.CLIENT)
 public class AstromineClientPackets {
 	public static final Identifier PRESSURE_UPDATE = AstromineCommon.identifier("pressure_update");
 
@@ -13,5 +20,21 @@ public class AstromineClientPackets {
 		ClientSidePacketRegistry.INSTANCE.register(PRESSURE_UPDATE, ((context, buffer) -> {
 			AstromineScreens.PRESSURE_TEXT.setText(String.valueOf(buffer.readString()));
 		}));
+
+		ClientSidePacketRegistry.INSTANCE.register(RocketEntity.ROCKET_SPAWN, (context, buffer) -> {
+			BlockPos pos = buffer.readBlockPos();
+			UUID uuid = buffer.readUuid();
+			int id = buffer.readInt();
+
+			context.getTaskQueue().execute(() -> {
+				RocketEntity rocketEntity = AstromineEntities.ROCKET.create(context.getPlayer().world);
+				rocketEntity.setUuid(uuid);
+				rocketEntity.requestTeleport(pos.getX(), pos.getY(), pos.getZ());
+				rocketEntity.updatePosition(pos.getX(), pos.getY(), pos.getZ());
+				rocketEntity.updateTrackedPosition(pos.getX(), pos.getY(), pos.getZ());
+				rocketEntity.setEntityId(id);
+				context.getPlayer().world.spawnEntity(rocketEntity);
+			});
+		});
 	}
 }
