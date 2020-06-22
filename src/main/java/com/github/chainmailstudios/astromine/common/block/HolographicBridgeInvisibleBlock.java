03549d9e0846cb6e915e23fc1bae3b8f9c43cb2b
vini2003 <vini.sch@outlook.com>
2020-06-22 15:08:40 -0300

    Component bridges.

--- src/main/java/com/github/chainmailstudios/astromine/common/block/HolographicBridgeInvisibleBlock.java
+++ src/main/java/com/github/chainmailstudios/astromine/common/block/HolographicBridgeInvisibleBlock.java
@@ -1,12 +1,16 @@
 package com.github.chainmailstudios.astromine.common.block;
 
-import com.github.chainmailstudios.astromine.common.bridge.HolographicBridgeManager;
+import com.github.chainmailstudios.astromine.component.WorldBridgeComponent;
+import com.github.chainmailstudios.astromine.registry.AstromineComponentTypes;
+import nerdhub.cardinal.components.api.component.ComponentProvider;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class HolographicBridgeInvisibleBlock extends Block {
 	public HolographicBridgeInvisibleBlock(AbstractBlock.Settings settings) {
@@ -31,11 +35,19 @@ public class HolographicBridgeInvisibleBlock extends Block {
 
 	@Override
 	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos position, ShapeContext context) {
-		return HolographicBridgeManager.getShape(world, position);
+		return getCollisionShape(state, world, position, context);
 	}
 
 	@Override
 	public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos position, ShapeContext context) {
-		return HolographicBridgeManager.getShape(world, position);
+		if (!(world instanceof World)) {
+			return VoxelShapes.empty();
+		} else {
+			ComponentProvider componentProvider = ComponentProvider.fromWorld((World) world);
+
+			WorldBridgeComponent bridgeComponent = componentProvider.getComponent(AstromineComponentTypes.WORLD_BRIDGE_COMPONENT);
+
+			return bridgeComponent.getShape(position);
+		}
 	}
 }
