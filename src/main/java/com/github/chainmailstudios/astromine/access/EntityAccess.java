465df8bdf15417a1e8e2a18636d6355a6136255c
shedaniel <daniel@shedaniel.me>
2020-06-26 18:14:44 +0800

    fix rocket dimension teleportation desync, fix rocket flint and steel click would also ride it.

---
+++ src/main/java/com/github/chainmailstudios/astromine/access/EntityAccess.java
@@ -0,0 +1,8 @@
+package com.github.chainmailstudios.astromine.access;
+
+import net.minecraft.entity.Entity;
+
+public interface EntityAccess {
+	Entity astromine_getLastVehicle();
+	void astromine_setLastVehicle(Entity lastVehicle);
+}
--- src/main/java/com/github/chainmailstudios/astromine/mixin/EntityMixin.java
+++ src/main/java/com/github/chainmailstudios/astromine/mixin/EntityMixin.java
@@ -1,32 +1,45 @@
 package com.github.chainmailstudios.astromine.mixin;
 
+import com.github.chainmailstudios.astromine.access.EntityAccess;
+import com.github.chainmailstudios.astromine.common.registry.DimensionLayerRegistry;
+import com.github.chainmailstudios.astromine.common.registry.GravityRegistry;
+import com.google.common.collect.Lists;
 import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
-
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import net.minecraft.entity.Entity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.github.chainmailstudios.astromine.common.registry.DimensionLayerRegistry;
-import com.github.chainmailstudios.astromine.common.registry.GravityRegistry;
-
-import com.google.common.collect.Lists;
 import java.util.List;
 
 @Mixin(Entity.class)
-public abstract class EntityMixin {
+public abstract class EntityMixin implements EntityAccess {
 	int lastY = 0;
 
 	@Shadow
 	public World world;
+
+	@Shadow public double lastRenderX;
+
+	@Unique Entity lastVehicle = null;
+
+	@Override
+	public Entity astromine_getLastVehicle() {
+		return lastVehicle;
+	}
+
+	@Override
+	public void astromine_setLastVehicle(Entity lastVehicle) {
+		this.lastVehicle = lastVehicle;
+	}
 
 	@ModifyVariable(at = @At("HEAD"), method = "handleFallDamage(FF)Z", index = 2)
 	float getDamageMultiplier(float damageMultiplier) {
@@ -35,50 +48,48 @@ public abstract class EntityMixin {
 		return damageMultiplier *= GravityRegistry.INSTANCE.get(world.getDimensionRegistryKey());
 	}
 
-	@Inject(at = @At("HEAD"), method = "tick()V")
+	@Inject(at = @At("HEAD"), method = "tickNetherPortal()V")
 	void onTick(CallbackInfo callbackInformation) {
 		Entity entity = (Entity) (Object) this;
 
-		if ((int) entity.getPos().getY() != lastY && !entity.world.isClient) {
+		if ((int) entity.getPos().getY() != lastY && !entity.world.isClient && entity.getVehicle() == null) {
 			lastY = (int) entity.getPos().getY();
 
-			int bY = DimensionLayerRegistry.INSTANCE.getLevel(DimensionLayerRegistry.Type.BOTTOM, entity.world.getDimensionRegistryKey());
-			int tY = DimensionLayerRegistry.INSTANCE.getLevel(DimensionLayerRegistry.Type.TOP, entity.world.getDimensionRegistryKey());
+			int bottomPortal = DimensionLayerRegistry.INSTANCE.getLevel(DimensionLayerRegistry.Type.BOTTOM, entity.world.getDimensionRegistryKey());
+			int topPortal = DimensionLayerRegistry.INSTANCE.getLevel(DimensionLayerRegistry.Type.TOP, entity.world.getDimensionRegistryKey());
 
-			if (lastY <= bY && bY != Integer.MIN_VALUE) {
+			if (lastY <= bottomPortal && bottomPortal != Integer.MIN_VALUE) {
 				RegistryKey<World> worldKey = RegistryKey.of(Registry.DIMENSION, DimensionLayerRegistry.INSTANCE.getDimension(DimensionLayerRegistry.Type.BOTTOM, entity.world.getDimensionRegistryKey()).getValue());
 
 				ServerWorld serverWorld = entity.world.getServer().getWorld(worldKey);
 
-				List<Entity> existingPassengers = entity.getPassengerList();
-				List<Entity> newPassengers = Lists.newArrayList();
-
-				for (Entity existingEntity : existingPassengers) {
-					newPassengers.add(FabricDimensions.teleport(existingEntity, serverWorld, DimensionLayerRegistry.INSTANCE.getPlacer(DimensionLayerRegistry.Type.BOTTOM, entity.world.getDimensionRegistryKey())));
-				}
+				List<Entity> existingPassengers = Lists.newArrayList(entity.getPassengerList());
 
 				Entity newEntity = FabricDimensions.teleport(entity, serverWorld, DimensionLayerRegistry.INSTANCE.getPlacer(DimensionLayerRegistry.Type.BOTTOM, entity.world.getDimensionRegistryKey()));
 
-				for (Entity newPassenger : newPassengers) {
-					newPassenger.startRiding(newEntity);
+				for (Entity existingEntity : existingPassengers) {
+					((EntityAccess) existingEntity).astromine_setLastVehicle(newEntity);
 				}
-			} else if (lastY >= tY && tY != Integer.MIN_VALUE) {
+			} else if (lastY >= topPortal && topPortal != Integer.MIN_VALUE) {
 				RegistryKey<World> worldKey = RegistryKey.of(Registry.DIMENSION, DimensionLayerRegistry.INSTANCE.getDimension(DimensionLayerRegistry.Type.TOP, entity.world.getDimensionRegistryKey()).getValue());
 
 				ServerWorld serverWorld = entity.world.getServer().getWorld(worldKey);
 
-				List<Entity> existingPassengers = entity.getPassengerList();
-				List<Entity> newPassengers = Lists.newArrayList();
-
-				for (Entity existingEntity : existingPassengers) {
-					newPassengers.add(FabricDimensions.teleport(existingEntity, serverWorld, DimensionLayerRegistry.INSTANCE.getPlacer(DimensionLayerRegistry.Type.TOP, entity.world.getDimensionRegistryKey())));
-				}
+				List<Entity> existingPassengers = Lists.newArrayList(entity.getPassengerList());
 
 				Entity newEntity = FabricDimensions.teleport(entity, serverWorld, DimensionLayerRegistry.INSTANCE.getPlacer(DimensionLayerRegistry.Type.TOP, entity.world.getDimensionRegistryKey()));
 
-				for (Entity newPassenger : newPassengers) {
-					newPassenger.startRiding(newEntity);
+				for (Entity existingEntity : existingPassengers) {
+					((EntityAccess) existingEntity).astromine_setLastVehicle(newEntity);
 				}
+			}
+		}
+
+		if (entity.getVehicle() != null) lastVehicle = null;
+		if (lastVehicle != null) {
+			if (lastVehicle.getEntityWorld().getRegistryKey().equals(entity.getEntityWorld().getRegistryKey())) {
+				entity.startRiding(lastVehicle);
+				lastVehicle = null;
 			}
 		}
 	}
