afadcc6055e65512e986fa5745923ef0f1c79d8f
shedaniel <daniel@shedaniel.me>
2020-06-28 22:44:00 +0800

    Fix rocket not continue going after going into space

--- src/main/java/com/github/chainmailstudios/astromine/mixin/EntityMixin.java
+++ src/main/java/com/github/chainmailstudios/astromine/mixin/EntityMixin.java
@@ -1,7 +1,16 @@
 package com.github.chainmailstudios.astromine.mixin;
 
+import com.github.chainmailstudios.astromine.access.EntityAccess;
+import com.github.chainmailstudios.astromine.common.registry.DimensionLayerRegistry;
+import com.github.chainmailstudios.astromine.common.registry.GravityRegistry;
+import com.google.common.collect.Lists;
 import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
-
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -10,17 +19,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.World;
-
-import com.github.chainmailstudios.astromine.access.EntityAccess;
-import com.github.chainmailstudios.astromine.common.registry.DimensionLayerRegistry;
-import com.github.chainmailstudios.astromine.common.registry.GravityRegistry;
-
-import com.google.common.collect.Lists;
 import java.util.List;
 
 @Mixin(Entity.class)
@@ -68,7 +66,16 @@ public abstract class EntityMixin implements EntityAccess {
 
 				List<Entity> existingPassengers = Lists.newArrayList(entity.getPassengerList());
 
+				List<DataTracker.Entry<?>> entries = Lists.newArrayList();
+				for (DataTracker.Entry<?> entry : entity.getDataTracker().getAllEntries()) {
+					entries.add(entry.copy());
+				}
+
 				Entity newEntity = FabricDimensions.teleport(entity, serverWorld, DimensionLayerRegistry.INSTANCE.getPlacer(DimensionLayerRegistry.Type.BOTTOM, entity.world.getDimensionRegistryKey()));
+
+				for (DataTracker.Entry entry : entries) {
+					newEntity.getDataTracker().set(entry.getData(), entry.get());
+				}
 
 				for (Entity existingEntity : existingPassengers) {
 					((EntityAccess) existingEntity).astromine_setLastVehicle(newEntity);
@@ -80,7 +87,16 @@ public abstract class EntityMixin implements EntityAccess {
 
 				List<Entity> existingPassengers = Lists.newArrayList(entity.getPassengerList());
 
+				List<DataTracker.Entry<?>> entries = Lists.newArrayList();
+				for (DataTracker.Entry<?> entry : entity.getDataTracker().getAllEntries()) {
+					entries.add(entry.copy());
+				}
+
 				Entity newEntity = FabricDimensions.teleport(entity, serverWorld, DimensionLayerRegistry.INSTANCE.getPlacer(DimensionLayerRegistry.Type.TOP, entity.world.getDimensionRegistryKey()));
+
+				for (DataTracker.Entry entry : entries) {
+					newEntity.getDataTracker().set(entry.getData(), entry.get());
+				}
 
 				for (Entity existingEntity : existingPassengers) {
 					((EntityAccess) existingEntity).astromine_setLastVehicle(newEntity);
