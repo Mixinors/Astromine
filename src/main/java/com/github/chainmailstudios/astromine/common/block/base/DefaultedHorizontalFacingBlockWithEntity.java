d9809bdbb00d9c9bb9de1dda0fe6da52a92eba8d
vini2003 <vini.sch@outlook.com>
2020-07-02 11:19:55 -0300

    if you have epilepsy machines will currently kill you

--- src/main/java/com/github/chainmailstudios/astromine/common/block/base/DefaultedHorizontalFacingBlockWithEntity.java
+++ src/main/java/com/github/chainmailstudios/astromine/common/block/base/DefaultedHorizontalFacingBlockWithEntity.java
@@ -1,21 +1,40 @@
 package com.github.chainmailstudios.astromine.common.block.base;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 
-public abstract class DefaultedHorizontalFacingBlockWithEntity extends DefaultedFacingBlockWithEntity {
+public abstract class DefaultedHorizontalFacingBlockWithEntity extends DefaultedBlockWithEntity {
 	public DefaultedHorizontalFacingBlockWithEntity(Settings settings) {
 		super(settings);
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		builder.add(getDirectionProperty());
+		super.appendProperties(builder);
 	}
 
 	@Override
 	public BlockState getPlacementState(ItemPlacementContext context) {
 		return super.getPlacementState(context).with(getDirectionProperty(), context.getPlayerFacing().getOpposite());
 	}
-	
+
 	@Override
+	public BlockState rotate(BlockState state, BlockRotation rotation) {
+		return state.with(getDirectionProperty(), rotation.rotate(state.get(getDirectionProperty())));
+	}
+
+	@Override
+	public BlockState mirror(BlockState state, BlockMirror mirror) {
+		return state.rotate(mirror.getRotation(state.get(getDirectionProperty())));
+	}
+
 	protected DirectionProperty getDirectionProperty() {
 		return Properties.HORIZONTAL_FACING;
 	}
