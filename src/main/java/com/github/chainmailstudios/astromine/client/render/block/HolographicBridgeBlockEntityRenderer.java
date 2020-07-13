18193def35f2d5e99c2799281d426f2b9fb02d9a
vini2003 <vini.sch@outlook.com>
2020-07-13 19:28:28 -0300

    Fix Holographic Bridge rendering regression.

--- src/main/java/com/github/chainmailstudios/astromine/client/render/block/HolographicBridgeBlockEntityRenderer.java
+++ src/main/java/com/github/chainmailstudios/astromine/client/render/block/HolographicBridgeBlockEntityRenderer.java
@@ -1,5 +1,7 @@
 package com.github.chainmailstudios.astromine.client.render.block;
 
+import com.github.chainmailstudios.astromine.client.render.layer.Layers;
+import com.github.chainmailstudios.astromine.common.block.HolographicBridgeProjectorBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.client.render.OverlayTexture;
@@ -26,38 +28,44 @@ public class HolographicBridgeBlockEntityRenderer extends BlockEntityRenderer<Ho
 
 	@Override
 	public void render(HolographicBridgeProjectorBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider provider, int light, int overlay) {
+		BlockState state = entity.getWorld().getBlockState(entity.getPos());
+
+		if (!(state.getBlock() instanceof HolographicBridgeProjectorBlock)) {
+			return;
+		}
+
 		if (entity.hasChild()) {
 			Vec3i pA = entity.getPos();
 
-			Direction direction = entity.getCachedState().get(HorizontalFacingBlock.FACING);
+			Direction direction = state.get(HorizontalFacingBlock.FACING);
 
-			int oX = direction == Direction.NORTH ? 1 : 0;
-			int oZ = direction == Direction.WEST ? 1 : 0;
+			int offsetX = direction == Direction.NORTH ? 1 : 0;
+			int offsetZ = direction == Direction.WEST ? 1 : 0;
 
-			if (entity.segments == null || entity.segments.isEmpty()) {
+			if (entity.segments == null || entity.segments.size() == 0) {
 				return;
 			}
 
 			Vector3f start = entity.segments.get(0);
 			Vector3f end = entity.segments.get(entity.segments.size() - 1);
 
-			VertexConsumer consumer = provider.getBuffer(SpinneryLayers.getFlat());
-
 			matrices.push();
 
-			float xA = start.getX() - pA.getX();
-			float xB = end.getX() - pA.getX();
+			VertexConsumer consumer = provider.getBuffer(Layers.getHolographicBridge());
 
-			float yA = start.getY() - pA.getY();
-			float yB = end.getY() - pA.getY();
+			float xA = end.getX() - pA.getX();
+			float xB = start.getX() - pA.getX();
 
-			float zA = start.getZ() - pA.getZ();
-			float zB = end.getZ() - pA.getZ();
+			float yA = end.getY() - pA.getY();
+			float yB = start.getY() - pA.getY();
+
+			float zA = end.getZ() - pA.getZ();
+			float zB = start.getZ() - pA.getZ();
 
 			consumer.vertex(matrices.peek().getModel(), xA, yA, zA).color(entity.color.R, entity.color.G, entity.color.B, entity.color.A).texture(0, 0).overlay(OverlayTexture.DEFAULT_UV).light(0x00f000f0).normal(matrices.peek().getNormal(), 0, 1, 0).next();
 			consumer.vertex(matrices.peek().getModel(), xB, yB, zB).color(entity.color.R, entity.color.G, entity.color.B, entity.color.A).texture(0, 1).overlay(OverlayTexture.DEFAULT_UV).light(0x00f000f0).normal(matrices.peek().getNormal(), 0, 1, 0).next();
-			consumer.vertex(matrices.peek().getModel(), xB + oX, yB, zB + oZ).color(entity.color.R, entity.color.G, entity.color.B, entity.color.A).texture(1, 1).overlay(OverlayTexture.DEFAULT_UV).light(0x00f000f0).normal(matrices.peek().getNormal(), 0, 1, 0).next();
-			consumer.vertex(matrices.peek().getModel(), xA + oX, yA, zA + oZ).color(entity.color.R, entity.color.G, entity.color.B, entity.color.A).texture(1, 0).overlay(OverlayTexture.DEFAULT_UV).light(0x00f000f0).normal(matrices.peek().getNormal(), 0, 1, 0).next();
+			consumer.vertex(matrices.peek().getModel(), xB + offsetX, yB, zB + offsetZ).color(entity.color.R, entity.color.G, entity.color.B, entity.color.A).texture(1, 1).overlay(OverlayTexture.DEFAULT_UV).light(0x00f000f0).normal(matrices.peek().getNormal(), 0, 1, 0).next();
+			consumer.vertex(matrices.peek().getModel(), xA + offsetX, yA, zA + offsetZ).color(entity.color.R, entity.color.G, entity.color.B, entity.color.A).texture(1, 0).overlay(OverlayTexture.DEFAULT_UV).light(0x00f000f0).normal(matrices.peek().getNormal(), 0, 1, 0).next();
 
 			matrices.pop();
 		}
