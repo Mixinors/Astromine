60ddf8a24edada4f4e291bcee1dcd32dc0a2e263
shedaniel <daniel@shedaniel.me>
2020-07-24 11:02:59 +0800

    Fix asteroid generation crash

--- src/main/java/com/github/chainmailstudios/astromine/client/registry/AsteroidOreRegistry.java
+++ src/main/java/com/github/chainmailstudios/astromine/client/registry/AsteroidOreRegistry.java
@@ -1,18 +1,18 @@
 /*
  * MIT License
- * 
+ *
  * Copyright (c) 2020 Chainmail Studios
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,36 +23,42 @@
  */
 package com.github.chainmailstudios.astromine.client.registry;
 
-import com.github.chainmailstudios.astromine.common.registry.base.MultiRegistry;
 import com.github.chainmailstudios.astromine.common.utilities.data.Range;
 import net.minecraft.block.Block;
-import net.minecraft.util.math.MathHelper;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
-public class AsteroidOreRegistry extends MultiRegistry<Integer, Block> {
+public class AsteroidOreRegistry {
 	public static final AsteroidOreRegistry INSTANCE = new AsteroidOreRegistry();
 
-	public final Map<Block, Integer> minimumDiameters = new HashMap<>();
-	public final Map<Block, Integer> maximumDiameters = new HashMap();
+	private final Map<Block, @Nullable Range<Integer>> diameters = new HashMap<>();
 
 	private AsteroidOreRegistry() {
 		// Locked.
 	}
 
-	public void register(Range<Integer> range, int minimumDiameter, int maximumDiameter, Block block) {
+	public void register(Range<Integer> range, Block block) {
 		if (range.getMinimum() > range.getMaximum()) {
 			range = Range.of(range.getMaximum(), range.getMinimum());
+		} else if (range.getMinimum().equals(range.getMaximum())) {
+			range = null;
 		}
 
-		for (int chance = range.getMinimum(); chance < range.getMaximum(); ++chance) {
-			this.register(chance, block);
+		if (range == null) {
+			diameters.remove(block);
+		} else {
+			diameters.put(block, range);
 		}
 	}
 
+	public Set<Block> keySet() {
+		return diameters.keySet();
+	}
+
 	public int getDiameter(Random random, Block block) {
-		return (int) MathHelper.clamp(maximumDiameters.get(block) * random.nextFloat(), minimumDiameters.get(block), maximumDiameters.get(block));
+		Range<Integer> range = diameters.get(block);
+		if (range == null) return 0;
+		return (int) ((range.getMaximum() - range.getMinimum()) * Objects.requireNonNull(random, "random").nextFloat() + range.getMinimum());
 	}
 }
