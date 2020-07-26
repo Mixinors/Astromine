b946cb05eac3573f2ca20dc8bf9b37e707ab3558
Jacob Farley <jacobfarley2017@gmail.com>
2020-07-26 14:38:57 -0500

    conveyor do the connect goodly

--- src/main/java/com/github/chainmailstudios/astromine/common/block/conveyor/ConveyorProperties.java
+++ src/main/java/com/github/chainmailstudios/astromine/common/block/conveyor/ConveyorProperties.java
@@ -30,6 +30,7 @@ public class ConveyorProperties {
 	public static final BooleanProperty FRONT = BooleanProperty.of("front");
 	public static final BooleanProperty LEFT = BooleanProperty.of("left");
 	public static final BooleanProperty RIGHT = BooleanProperty.of("right");
+	public static final BooleanProperty BACK = BooleanProperty.of("back");
 	public static final BooleanProperty UP = BooleanProperty.of("up");
 	public static final BooleanProperty CONVEYOR = BooleanProperty.of("conveyor");
 	public static final BooleanProperty FLOOR = BooleanProperty.of("floor");
