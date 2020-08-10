58d1f9125a766dc53b90d0cb29164246782303c2
shedaniel <daniel@shedaniel.me>
2020-08-10 21:06:54 +0800

    proper buildscript

--- src/main/java/com/github/chainmailstudios/astromine/common/registry/DimensionLayerRegistry.java
+++ src/main/java/com/github/chainmailstudios/astromine/common/registry/DimensionLayerRegistry.java
@@ -24,7 +24,7 @@
 
 package com.github.chainmailstudios.astromine.common.registry;
 
-import net.fabricmc.fabric.api.dimension.v1.EntityPlacer;
+//import net.fabricmc.fabric.api.dimension.v1.EntityPlacer;
 import net.minecraft.util.Pair;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.dimension.DimensionType;
@@ -36,12 +36,13 @@ public class DimensionLayerRegistry {
 	public static final DimensionLayerRegistry INSTANCE = new DimensionLayerRegistry();
 	private final Map<RegistryKey<DimensionType>, Pair<Integer, RegistryKey<DimensionType>>> TOP_ENTRIES = new HashMap<>();
 	private final Map<RegistryKey<DimensionType>, Pair<Integer, RegistryKey<DimensionType>>> BOTTOM_ENTRIES = new HashMap<>();
-	private final Map<RegistryKey<DimensionType>, Pair<EntityPlacer, EntityPlacer>> PLACERS = new HashMap<>();
+//	private final Map<RegistryKey<DimensionType>, Pair<EntityPlacer, EntityPlacer>> PLACERS = new HashMap<>();
 
 	private DimensionLayerRegistry() {
 		// Unused.
 	}
 
+	/*
 	public void register(Type type, RegistryKey<DimensionType> dimension, Integer levelY, RegistryKey<DimensionType> newDimension, EntityPlacer placer) {
 		final Map<RegistryKey<DimensionType>, Pair<Integer, RegistryKey<DimensionType>>> ENTRIES = type == Type.TOP ? this.TOP_ENTRIES : this.BOTTOM_ENTRIES;
 
@@ -52,7 +53,7 @@ public class DimensionLayerRegistry {
 		} else {
 			PLACERS.put(dimension, new Pair<>(type == Type.TOP ? placer : null, type == Type.BOTTOM ? placer : null));
 		}
-	}
+	}*/
 
 	public int getLevel(Type type, RegistryKey<DimensionType> dimension) {
 		final Map<RegistryKey<DimensionType>, Pair<Integer, RegistryKey<DimensionType>>> ENTRIES = type == Type.TOP ? this.TOP_ENTRIES : this.BOTTOM_ENTRIES;
@@ -70,9 +71,9 @@ public class DimensionLayerRegistry {
 		return pair == null ? null : pair.getRight();
 	}
 
-	public EntityPlacer getPlacer(Type type, RegistryKey<DimensionType> dimension) {
-		return type == Type.TOP ? PLACERS.get(dimension).getLeft() : PLACERS.get(dimension).getRight();
-	}
+//	public EntityPlacer getPlacer(Type type, RegistryKey<DimensionType> dimension) {
+//		return type == Type.TOP ? PLACERS.get(dimension).getLeft() : PLACERS.get(dimension).getRight();
+//	}
 
 	public enum Type {
 		TOP,
