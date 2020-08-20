ccc384bf410dd2eeec5b20060a0e7a1ec2d9cabe
Jacob Farley <jacobfarley2017@gmail.com>
2020-08-20 14:01:37 -0500

    Hi yes I moved crap back where it belongs and now the lines do the render.

--- astromine-core/src/main/java/com/github/chainmailstudios/astromine/common/component/world/WorldAtmosphereComponent.java
+++ astromine-core/src/main/java/com/github/chainmailstudios/astromine/common/component/world/WorldAtmosphereComponent.java
@@ -24,8 +24,11 @@
 
 package com.github.chainmailstudios.astromine.common.component.world;
 
-import com.github.chainmailstudios.astromine.common.volume.fluid.FluidVolume;
-import com.google.common.collect.Lists;
+import com.github.chainmailstudios.astromine.client.cca.FuckingHellCCA;
+import com.github.vini2003.blade.common.utilities.Networks;
+import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
+import net.fabricmc.fabric.impl.networking.ServerSidePacketRegistryImpl;
+import net.minecraft.block.AirBlock;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.Tickable;
 import net.minecraft.util.math.BlockPos;
@@ -34,104 +37,118 @@ import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 
 import com.github.chainmailstudios.astromine.common.fraction.Fraction;
+import com.github.chainmailstudios.astromine.common.volume.fluid.FluidVolume;
 import com.github.chainmailstudios.astromine.registry.AstromineDimensions;
 import nerdhub.cardinal.components.api.component.Component;
 
+import com.google.common.collect.Lists;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class WorldAtmosphereComponent implements Component, Tickable {
-	private final List<Direction> directions = Lists.newArrayList(Direction.values());
+		private final List<Direction> directions = Lists.newArrayList(Direction.values());
 
-	private final Map<BlockPos, FluidVolume> volumes = new ConcurrentHashMap<>();
+		private final Map<BlockPos, FluidVolume> volumes = new ConcurrentHashMap<>();
 
-	private final World world;
+		private final World world;
 
-	public WorldAtmosphereComponent(World world) {
-		this.world = world;
-	}
+		public WorldAtmosphereComponent(World world) {
+			this.world = world;
+		}
 
-	public World getWorld() {
-		return world;
-	}
+		public World getWorld() {
+			return world;
+		}
+
+		public FluidVolume get(BlockPos position) {
+			RegistryKey<World> key = world.getRegistryKey();
+
+			if (!AstromineDimensions.isAstromine(key) && !volumes.containsKey(position)) {
+				return FluidVolume.oxygen();
+			} else {
+				return volumes.getOrDefault(position, FluidVolume.empty());
+			}
+		}
 
 	public void add(BlockPos blockPos, FluidVolume volume) {
 		volumes.put(blockPos, volume);
+
+		if (!world.isClient) {
+			world.getPlayers().forEach((player) -> {
+				ServerSidePacketRegistry.INSTANCE.sendToPlayer(player, FuckingHellCCA.FUCKS_GIVEN, FuckingHellCCA.ofFucksGiven(blockPos, volume));
+			});
+		}
 	}
 
 	public void remove(BlockPos blockPos) {
 		volumes.remove(blockPos);
-	}
 
-	public FluidVolume get(BlockPos position) {
-		RegistryKey<World> key = world.getRegistryKey();
-
-		if (!AstromineDimensions.isAstromine(key) && !volumes.containsKey(position)) {
-			return FluidVolume.oxygen();
-		} else {
-			return volumes.getOrDefault(position, FluidVolume.empty());
+		if (!world.isClient) {
+			world.getPlayers().forEach((player) -> {
+				ServerSidePacketRegistry.INSTANCE.sendToPlayer(player, FuckingHellCCA.FUCKS_TAKEN, FuckingHellCCA.ofFucksTaken(blockPos));
+			});
 		}
 	}
 
-	@Override
-	public void tick() {
-		for (Map.Entry<BlockPos, FluidVolume> pair : volumes.entrySet()) {
-			BlockPos centerPos = pair.getKey();
+		@Override
+		public void tick() {
+			for (Map.Entry<BlockPos, FluidVolume> pair : volumes.entrySet()) {
+				BlockPos centerPos = pair.getKey();
 
-			FluidVolume centerVolume = pair.getValue();
+				FluidVolume centerVolume = pair.getValue();
 
-			centerVolume.extractVolume(Fraction.of(1, 2048));
+				centerVolume.extractVolume(Fraction.of(1, 2048));
 
-			if (centerVolume.isEmpty()) {
-				remove(centerPos);
-			}
+				if (centerVolume.isEmpty()) {
+					remove(centerPos);
+				}
 
-			Collections.shuffle(directions);
+				Collections.shuffle(directions);
 
-			for (Direction direction : directions) {
-				BlockPos sidePos = centerPos.offset(direction);
+				for (Direction direction : directions) {
+					BlockPos sidePos = centerPos.offset(direction);
 
-				FluidVolume sideVolume = get(sidePos);
+					FluidVolume sideVolume = get(sidePos);
 
-				if (world.getBlockState(sidePos).isAir() && (sideVolume.isEmpty() || sideVolume.equalsFluid(centerVolume)) && centerVolume.hasStored(Fraction.bottle()) && sideVolume.isSmallerThan(centerVolume)) {
-					centerVolume.pushVolume(sideVolume, Fraction.bottle());
+					if (world.getBlockState(sidePos).isAir() && (sideVolume.isEmpty() || sideVolume.equalsFluid(centerVolume)) && centerVolume.hasStored(Fraction.bottle()) && sideVolume.isSmallerThan(centerVolume)) {
+						centerVolume.pushVolume(sideVolume, Fraction.bottle());
 
-					add(sidePos, sideVolume);
+						add(sidePos, sideVolume);
+					}
 				}
 			}
 		}
-	}
 
-	@Override
-	public CompoundTag toTag(CompoundTag tag) {
-		CompoundTag dataTag = new CompoundTag();
+		@Override
+		public CompoundTag toTag(CompoundTag tag) {
+			CompoundTag dataTag = new CompoundTag();
 
-		int i = 0;
+			int i = 0;
 
-		for (Map.Entry<BlockPos, FluidVolume> entry : volumes.entrySet()) {
-			CompoundTag pointTag = new CompoundTag();
-			pointTag.putLong("pos", entry.getKey().asLong());
-			pointTag.put("volume", entry.getValue().toTag(new CompoundTag()));
+			for (Map.Entry<BlockPos, FluidVolume> entry : volumes.entrySet()) {
+				CompoundTag pointTag = new CompoundTag();
+				pointTag.putLong("pos", entry.getKey().asLong());
+				pointTag.put("volume", entry.getValue().toTag(new CompoundTag()));
 
-			dataTag.put(String.valueOf(i), pointTag);
-			++i;
+				dataTag.put(String.valueOf(i), pointTag);
+				++i;
+			}
+
+			tag.put("data", dataTag);
+
+			return tag;
 		}
 
-		tag.put("data", dataTag);
+		@Override
+		public void fromTag(CompoundTag tag) {
+			CompoundTag dataTag = tag.getCompound("data");
 
-		return tag;
-	}
+			for (String key : dataTag.getKeys()) {
+				CompoundTag pointTag = dataTag.getCompound(key);
 
-	@Override
-	public void fromTag(CompoundTag tag) {
-		CompoundTag dataTag = tag.getCompound("data");
-
-		for (String key : dataTag.getKeys()) {
-			CompoundTag pointTag = dataTag.getCompound(key);
-
-			volumes.put(BlockPos.fromLong(pointTag.getLong("pos")), FluidVolume.fromTag(pointTag.getCompound("volume")));
+				volumes.put(BlockPos.fromLong(pointTag.getLong("pos")), FluidVolume.fromTag(pointTag.getCompound("volume")));
+			}
 		}
 	}
-}
