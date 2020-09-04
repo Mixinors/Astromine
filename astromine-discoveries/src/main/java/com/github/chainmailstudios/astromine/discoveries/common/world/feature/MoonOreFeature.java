9d6e3b7dd69ac85cbf41b36be361eb1c69635a07
SuperCoder7979 <k.pranav@gmail.com>
2020-09-04 12:44:21 -0400

    improve Lunum generation

--- astromine-discoveries/src/main/java/com/github/chainmailstudios/astromine/discoveries/common/world/feature/MoonOreFeature.java
+++ astromine-discoveries/src/main/java/com/github/chainmailstudios/astromine/discoveries/common/world/feature/MoonOreFeature.java
@@ -1,12 +1,18 @@
 package com.github.chainmailstudios.astromine.discoveries.common.world.feature;
 
 import java.util.Random;
+import java.util.stream.Collectors;
 
+import com.github.chainmailstudios.astromine.discoveries.registry.AstromineDiscoveriesBlocks;
 import com.mojang.serialization.Codec;
+import com.terraformersmc.shapes.api.Position;
+import com.terraformersmc.shapes.api.Quaternion;
+import com.terraformersmc.shapes.api.Shape;
+import com.terraformersmc.shapes.impl.Shapes;
+import com.terraformersmc.shapes.impl.layer.transform.RotateLayer;
+import com.terraformersmc.shapes.impl.layer.transform.TranslateLayer;
 
-import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
@@ -19,16 +25,17 @@ public class MoonOreFeature extends Feature<DefaultFeatureConfig> {
 
 	@Override
 	public boolean generate(StructureWorldAccess world, ChunkGenerator chunkGenerator, Random random, BlockPos pos, DefaultFeatureConfig featureConfig) {
-		if (world.getBlockState(pos).isOpaque()) {
-			world.setBlockState(pos, Blocks.DIAMOND_ORE.getDefaultState(), 3);
-		}
+		Shape vein = Shapes.ellipsoid(random.nextFloat() * 6, random.nextFloat() * 6, random.nextFloat() * 6)
+				.applyLayer(RotateLayer.of(Quaternion.of(random.nextDouble() * 360, random.nextDouble() * 360, random.nextDouble() * 360, true)))
+				.applyLayer(TranslateLayer.of(Position.of(pos)));
 
-		for (Direction direction : Direction.values()) {
-			BlockPos local = pos.offset(direction);
+		for (Position streamPosition : vein.stream().collect(Collectors.toSet())) {
+			BlockPos orePos = streamPosition.toBlockPos();
 
-			if (world.getBlockState(local).isOpaque()) {
-				// TODO: replace with lunum
-				world.setBlockState(local, Blocks.DIAMOND_ORE.getDefaultState(), 3);
+			if (world.getBlockState(orePos).getBlock() == AstromineDiscoveriesBlocks.MOON_STONE) {
+				if (random.nextInt(24) == 0) {
+					world.setBlockState(orePos, AstromineDiscoveriesBlocks.MOON_LUNUM_ORE.getDefaultState(), 0b0110100);
+				}
 			}
 		}
 
