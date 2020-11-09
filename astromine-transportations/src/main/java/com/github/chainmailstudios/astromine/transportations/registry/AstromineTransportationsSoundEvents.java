e563d5f29b77ee404f61d8ea01e786aff6cce678
Shnupbups <shnupbups@gmail.com>
2020-11-09 17:33:03 +1100

    Update AstromineTransportationsSoundEvents.java
    added no-op initialize method, should fix #283

--- astromine-transportations/src/main/java/com/github/chainmailstudios/astromine/transportations/registry/AstromineTransportationsSoundEvents.java
+++ astromine-transportations/src/main/java/com/github/chainmailstudios/astromine/transportations/registry/AstromineTransportationsSoundEvents.java
@@ -31,4 +31,8 @@ import com.github.chainmailstudios.astromine.registry.AstromineSoundEvents;
 public class AstromineTransportationsSoundEvents extends AstromineSoundEvents {
 	public static final SoundEvent MACHINE_CLICK = register("block.machine.click");
 	public static final SoundEvent INCINERATE = register("block.incinerator.incinerate");
+
+	public static void initialize() {
+
+	}
 }
