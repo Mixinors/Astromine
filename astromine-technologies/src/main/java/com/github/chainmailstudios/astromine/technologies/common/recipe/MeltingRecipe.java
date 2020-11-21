1ea95379a89d887d007160c779f2b58122afd07a
Neubulae <neubulaeko@outlook.com>
2020-11-22 04:03:37 +0800

    and this

--- astromine-technologies/src/main/java/com/github/chainmailstudios/astromine/technologies/common/recipe/MeltingRecipe.java
+++ astromine-technologies/src/main/java/com/github/chainmailstudios/astromine/technologies/common/recipe/MeltingRecipe.java
@@ -171,7 +171,7 @@ public final class MeltingRecipe implements EnergyConsumingRecipe<Inventory> {
 	}
 
 	public static final class Serializer implements RecipeSerializer<MeltingRecipe> {
-		public static final Identifier ID = AstromineCommon.identifier("solidifying");
+		public static final Identifier ID = AstromineCommon.identifier("melting");
 
 		public static final Serializer INSTANCE = new Serializer();
 
