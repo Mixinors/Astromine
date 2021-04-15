011b9523f8d3d36c3d1c89e7a8ba64427150931f
shedaniel <daniel@shedaniel.me>
2021-04-15 13:23:31 +0800

    Welcome to 2021, change license headers

--- astromine-foundations/src/main/java/com/github/chainmailstudios/astromine/foundations/registry/AstromineFoundationsConfig.java
+++ astromine-foundations/src/main/java/com/github/chainmailstudios/astromine/foundations/registry/AstromineFoundationsConfig.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 4040 Chainmail Studios
+ * Copyright (c) 2020, 2021 Mixinors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
