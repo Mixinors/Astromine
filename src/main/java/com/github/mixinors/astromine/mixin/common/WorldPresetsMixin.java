b8312f7735f85548d135d8e5ad13493386b0fbdf
Vinícius S <vini.sch@outlook.com>
2023-05-01 17:36:56 -0300

    fix: dimensions being improperly registered

--- src/main/java/com/github/mixinors/astromine/mixin/common/WorldPresetsMixin.java
+++ src/main/java/com/github/mixinors/astromine/mixin/common/WorldPresetsMixin.java
@@ -1,5 +1,6 @@
 package com.github.mixinors.astromine.mixin.common;
 
+import com.github.mixinors.astromine.registry.common.AMDimensionOptions;
 import com.github.mixinors.astromine.registry.common.AMWorlds;
 import net.minecraft.world.dimension.DimensionOptions;
 import net.minecraft.world.gen.WorldPreset;
@@ -16,6 +17,9 @@ public class WorldPresetsMixin {
 	private void astromine$addAstromineDimensions(DimensionOptions dimensionOptions, CallbackInfoReturnable<WorldPreset> cir) {
 		var preset = cir.getReturnValue();
 		preset.dimensions = new HashMap<>(preset.dimensions);
-		AMWorlds.DIMENSIONS.forEach((key, value) -> preset.dimensions.put(key, value.get()));
+		
+		preset.dimensions.put(AMWorlds.ROCKET_INTERIORS_DIMENSION_OPTIONS_KEY, AMDimensionOptions.ROCKET_INTERIORS);
+		preset.dimensions.put(AMWorlds.MOON_DIMENSION_OPTIONS_KEY, AMDimensionOptions.MOON);
+		preset.dimensions.put(AMWorlds.EARTH_ORBIT_DIMENSION_OPTIONS_KEY, AMDimensionOptions.EARTH_ORBIT);
 	}
 }
--- src/main/java/com/github/mixinors/astromine/registry/common/AMWorlds.java
+++ src/main/java/com/github/mixinors/astromine/registry/common/AMWorlds.java
@@ -39,6 +39,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.dimension.DimensionOptions;
 import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.gen.WorldPresets;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 
 import java.util.*;
@@ -46,62 +47,20 @@ import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
 public class AMWorlds {
-	public static final Map<RegistryKey<DimensionOptions>, Supplier<DimensionOptions>> DIMENSIONS = new HashMap<>();
-	private static final Set<RegistryKey<World>> KEYS = new HashSet<>();
-	private static final DimensionType.MonsterSettings MONSTER_SETTINGS = new DimensionType.MonsterSettings(false, true, UniformIntProvider.create(0, 7), 7);
+	public static final RegistryKey<World> ROCKET_INTERIORS = createWorldKey(AMCommon.id("rocket_interiors"));
+	public static final RegistryKey<DimensionType> ROCKET_INTERIORS_DIMENSION_TYPE_KEY = createTypeKey(AMCommon.id("rocket_interiors"));
+	public static final RegistryKey<DimensionOptions> ROCKET_INTERIORS_DIMENSION_OPTIONS_KEY = createOptionsKey(AMCommon.id("rocket_interiors"));
 	
-	public static final RegistryKey<World> ROCKET_INTERIORS = registerDimension(AMCommon.id("rocket_interiors"), (s, b) -> new RocketInteriorsChunkGenerator(s, new RocketInteriorsBiomeSource(b)),
-			15000,
-			false,
-			false,
-			false,
-			false,
-			1.0,
-			false,
-			false,
-			0,
-			96,
-			96,
-			AMTagKeys.BlockTags.INFINIBURN_SPACE,
-			0F,
-			MONSTER_SETTINGS
-	);
+	public static final RegistryKey<World> MOON = createWorldKey(AMCommon.id("moon"));
+	public static final RegistryKey<DimensionType> MOON_DIMENSION_TYPE_KEY = createTypeKey(AMCommon.id("moon"));
+	public static final RegistryKey<DimensionOptions> MOON_DIMENSION_OPTIONS_KEY = createOptionsKey(AMCommon.id("moon"));
 	
-	public static final RegistryKey<World> MOON = registerDimension(AMCommon.id("moon"), (s, b) -> new MoonChunkGenerator(s, new MoonBiomeSource(b)),
-			15000,
-			true,
-			false,
-			false,
-			false,
-			1.0,
-			false,
-			false,
-			-64,
-			384,
-			382,
-			AMTagKeys.BlockTags.INFINIBURN_SPACE,
-			0.4F,
-			MONSTER_SETTINGS
-	);
-	
-	public static final RegistryKey<World> EARTH_ORBIT = registerDimension(AMCommon.id("earth_orbit"), (s, b) -> new EarthOrbitChunkGenerator(s, new EarthOrbitBiomeSource(b)),
-			15000,
-			false,
-			false,
-			false,
-			false,
-			1.0,
-			false,
-			false,
-			0,
-			512,
-			512,
-			AMTagKeys.BlockTags.INFINIBURN_SPACE,
-			0.025F,
-			MONSTER_SETTINGS
-	);
+	public static final RegistryKey<World> EARTH_ORBIT = createWorldKey(AMCommon.id("earth_orbit"));
+	public static final RegistryKey<DimensionType> EARTH_ORBIT_DIMENSION_TYPE_KEY = createTypeKey(AMCommon.id("earth_orbit"));
+	public static final RegistryKey<DimensionOptions> EARTH_ORBIT_DIMENSION_OPTIONS_KEY = createOptionsKey(AMCommon.id("earth_orbit"));
 	
 	public static void init() {
+	
 	}
 	
 	public static boolean isVacuum(RegistryEntry<DimensionType> dimensionType) {
@@ -109,35 +68,7 @@ public class AMWorlds {
 	}
 	
 	public static boolean isAstromine(RegistryKey<World> key) {
-		return KEYS.contains(key);
-	}
-	
-	public static RegistryKey<World> registerDimension(Identifier id, BiFunction<Registry<StructureSet>, Registry<Biome>, ChunkGenerator> chunkGenerator, long fixedTime, boolean hasSkylight, boolean hasCeiling, boolean ultrawarm, boolean natural, double coordinateScale, boolean bedWorks, boolean piglinAndRespawnAnchorSafe, int minY, int height, int logicalHeight, TagKey<Block> infiniburn, float ambientLight, DimensionType.MonsterSettings monsterSettings) {
-		var dimWorldKey = createWorldKey(id);
-		var dimTypeKey = createTypeKey(id);
-		var dimOptionsKey = createOptionsKey(id);
-		
-		BuiltinRegistries.add(BuiltinRegistries.DIMENSION_TYPE, dimTypeKey, new DimensionType(
-				OptionalLong.of(fixedTime),
-				hasSkylight,
-				hasCeiling,
-				ultrawarm,
-				natural,
-				coordinateScale,
-				bedWorks,
-				piglinAndRespawnAnchorSafe,
-				minY,
-				height,
-				logicalHeight,
-				infiniburn,
-				AMCommon.id("space"),
-				ambientLight,
-				monsterSettings
-		));
-		
-		DIMENSIONS.put(dimOptionsKey, () -> new DimensionOptions(BuiltinRegistries.DIMENSION_TYPE.getOrCreateEntry(dimTypeKey), chunkGenerator.apply(BuiltinRegistries.STRUCTURE_SET, BuiltinRegistries.BIOME)));
-		
-		return dimWorldKey;
+		return key.equals(ROCKET_INTERIORS) || key.equals(MOON) || key.equals(EARTH_ORBIT);
 	}
 	
 	public static RegistryKey<DimensionOptions> createOptionsKey(Identifier id) {
@@ -149,8 +80,6 @@ public class AMWorlds {
 	}
 	
 	public static RegistryKey<World> createWorldKey(Identifier id) {
-		var key = RegistryKey.of(Registry.WORLD_KEY, id);
-		KEYS.add(key);
-		return key;
+		return RegistryKey.of(Registry.WORLD_KEY, id);
 	}
 }
